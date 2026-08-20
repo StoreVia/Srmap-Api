42c4c56480e2a0d5633cf19a2935838fa3c8ee4a
N.D Brahmendra <92256188+StoreVia@users.noreply.github.com>
2026-08-20 11:46:10 +0530

    Fix next.config.js

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const isDev = process.env.NODE_ENV === "development";
 const config = {
   reactStrictMode: !isDev,
   trailingSlash: false,
+  allowedDevOrigins: ['100.112.217.28'],
   devIndicators: false,
   images: {
     unoptimized: isDev,
@@ -12,6 +13,7 @@ const config = {
   typescript: {
     ignoreBuildErrors: isDev,
   },
+  serverExternalPackages: ["sharp", "tfjs-tflite-node", "@tensorflow/tfjs"],
 };
 
 if (isDev) {
