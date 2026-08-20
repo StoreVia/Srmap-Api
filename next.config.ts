849f09332c44427c1d2badff1299c20eeb7b4eff
N.D Brahmendra <92256188+StoreVia@users.noreply.github.com>
2026-08-20 13:56:03 +0530

    [skip ci]

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const isDev = process.env.NODE_ENV === "development";
 const config = {
   reactStrictMode: !isDev,
   trailingSlash: false,
-  allowedDevOrigins: ['100.112.217.28'],
   devIndicators: false,
   images: {
     unoptimized: isDev,
