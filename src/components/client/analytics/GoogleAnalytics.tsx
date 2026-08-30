af2c2d670ff0b00f79fbc4a5dedaac17c0a84a46
N.D Brahmendra <92256188+StoreVia@users.noreply.github.com>
2026-08-30 15:34:59 +0530

    Updated Google Analytics ID

--- src/components/client/analytics/GoogleAnalytics.tsx
+++ src/components/client/analytics/GoogleAnalytics.tsx
@@ -5,14 +5,14 @@ const GoogleAnalytics = () => {
         <>
             <Script
                 strategy="lazyOnload"
-                src={`https://www.googletagmanager.com/gtag/js?id=G-S21W7V6025`}
+                src={`https://www.googletagmanager.com/gtag/js?id=G-J9035T5XDT`}
             />
             <Script id="" strategy="lazyOnload">
                 {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'G-S21W7V6025');
+                gtag('config', 'G-J9035T5XDT');
           `}
             </Script>
         </>
